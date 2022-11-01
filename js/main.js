89d68a20926b460c98ec518790df7ab1b916611a
ippoRulez <ippolitodonato@gmail.com>
2022-11-01 11:33:25 +0100

    refined some logic and added more error handling

--- js/main.js
+++ js/main.js
@@ -20,65 +20,65 @@ let discountedPrice;
 buyBtnEl.addEventListener('click', function () {
 	const kmNeeded = parseInt(kmEl.value);
 	const age = parseInt(ageEl.value);
+	const ticketPrice = kmPrice * kmNeeded;
+	let discountApplied = discountEl.value;
 
 	//Controllo i dati inseriti, altrimenti li faccio reinserire
 
 	if (isNaN(age) || isNaN(kmNeeded)) {
 		alert('Attenzione! Rincontrollare dati inseriti');
-	}
-
-	let discountApplied = discountEl.value;
-
-	const ticketPrice = kmPrice * kmNeeded;
-
-	if (
-		(age < 18 && discountApplied != 0.8) ||
-		(age > 65 && discountApplied != 0.6)
-	) {
-		alert('Lo sconto applicato non corrisponde');
-	}
-
-	//applicare lo sconto corretto se selezionato
-
-	if (discountEl.value === '') {
-		discountedPrice = ticketPrice;
-		discountDisplay.innerText = 'Base';
 	} else {
-		discountedPrice = ticketPrice * discountApplied;
+		//se non si inserisce il nome, segnare come ospite e non inserire numero carta
+		//stampo il nome prima dell'errore dello sconto in quanto è un elemento separato dal resto della logica
+		if (nameEl.value == '') {
+			nameDisplay.innerText = 'Guest';
+			cardNumberDisplay.innerText = 'Nessuna carta';
+		} else {
+			nameDisplay.innerHTML = nameEl.value;
+			cardNumberDisplay.innerText = cardNumber;
+		}
 
-		//stampare sconto giusti in base a quello selezionato
+		if (
+			(age < 18 && discountApplied != 0.8) ||
+			(age > 65 && discountApplied != 0.6) ||
+			(age >= 18 && age <= 65 && discountApplied != '')
+		) {
+			alert('Lo sconto applicato non corrisponde');
+			//se i valori inseriti non sono idoneai stampo dei trattini placeholder
+			discountDisplay.innerText = '--';
+			totalDisplay.innerHTML = '--&euro;';
+			seatDisplay.innerText = '--';
+		} else {
+			//applicare lo sconto corretto
+			if (discountEl.value === '') {
+				discountedPrice = ticketPrice;
+			} else {
+				discountedPrice = ticketPrice * discountApplied;
+			}
+			//assegno il posto insieme al prezzo totale scontato
+			seatDisplay.innerText = seatNumber;
+			totalDisplay.innerHTML = `${discountedPrice.toFixed(2)}&euro;`;
 
-		if (discountApplied == 0.8) {
-			discountDisplay.innerHTML = 'Minnorenni';
-		} else if (discountApplied == 0.6) {
-			discountDisplay.innerHTML = 'Over 65';
+			//stampa sconto giusto in base a quello selezionato
+			if (discountApplied == 0.8) {
+				discountDisplay.innerText = 'Under 18';
+			} else if (discountApplied == 0.6) {
+				discountDisplay.innerText = 'Over 65';
+			} else {
+				discountDisplay.innerText = 'Base';
+			}
 		}
 	}
-
-	//se non si ha il prezzo perche i valori non sono numeri, si inseriscono dei trattini
-
-	if (isNaN(discountedPrice)) {
-		totalDisplay.innerHTML = '--&euro;';
-	} else {
-		totalDisplay.innerHTML = `${discountedPrice.toFixed(2)}&euro;`;
-	}
-
-	//se non si inserisce il nome, segnare come ospite e non inserire numero carta
-
-	if (nameEl.value == '') {
-		nameDisplay.innerText = 'Guest';
-		cardNumberDisplay.innerText = 'Nessuna carta';
-	} else {
-		nameDisplay.innerHTML = nameEl.value;
-		cardNumberDisplay.innerText = cardNumber;
-	}
-
-	//assegno un posto a prescindere da tutto
-	seatDisplay.innerText = seatNumber;
 });
 
 delBtnEl.addEventListener('click', function () {
 	nameEl.value = '';
 	ageEl.value = '';
 	kmEl.value = '';
+
+	nameDisplay.innerText = '';
+	discountDisplay.innerText = '';
+	cardNumberDisplay.innerText = '';
+	seatDisplay.innerText = '';
+	totalDisplay.innerText = '';
 });
